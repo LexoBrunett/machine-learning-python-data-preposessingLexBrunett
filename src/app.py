6b8b1d5249cfd8004704f9b8731e346ab15dc72b
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-10-29 20:46:00 +0000

    app

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data.head()
+
+import pandas as pd
+
+data_adquiered = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+data_adquiered.to_csv("../data/raw/total_data.csv", index = False)
+data_adquiered.head()
